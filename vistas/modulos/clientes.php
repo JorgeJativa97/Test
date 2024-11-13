ac75ce23aab59588f1901f9bc586191cad6ac7d3
JorgeJativa97 <georgoandres.jm@gmail.com>
2024-11-12 23:42:05 -0500

    prueba test 10

--- vistas/modulos/clientes.php
+++ vistas/modulos/clientes.php
@@ -42,7 +42,7 @@ if($_SESSION["perfil"] == "Especial"){
   
         <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarCliente">
           
-          Agregar Cliente
+          Agregar cliente
 
         </button>
 
